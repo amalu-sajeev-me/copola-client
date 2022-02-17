60c02361ef15324c3fc9dce2ea8a49f9a48cd948
Amalu Sajeev <amalu.sajeev.me@gmail.com>
2022-02-17 20:41:22 +0530

    home logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Image from "react-bootstrap/Image";
+import logo from "./assets/logo-black.png";
 import { Navbar, Container, Nav } from "react-bootstrap";
 
 const Title = styled.h1`
@@ -7,7 +9,10 @@ const Title = styled.h1`
   text-align: center;
   color: palevioletred;
 `;
-
+const Para = styled.p`
+  color: darkgrey;
+  text-align: center;
+`;
 const Wrapper = styled.section`
   padding: 4em;
   background: papayawhip;
@@ -18,7 +23,9 @@ function App() {
     <>
       <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand href="/">Home</Navbar.Brand>
+          <Navbar.Brand href="/">
+            <Image src={logo} height="178" />
+          </Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#about">About</Nav.Link>
             <Nav.Link href="#contact">contact</Nav.Link>
@@ -33,6 +40,10 @@ function App() {
       </Navbar>
       <Wrapper>
         <Title>COPOLA!</Title>
+        <Para>
+          A project to create a platform where skilled or unskilled laborers can
+          find jobs.
+        </Para>
       </Wrapper>
     </>
   );
